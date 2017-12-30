fb48a32a492557b612588067beb33ab6ab449b7c
Igordr1999 <Igordr1999@yandex.ru>
2017-12-30 21:04:15 +0300

    debug = false

--- date29/settings.py
+++ date29/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'csc39$dmh*jpfz)iwcm9e)&i8&&^nam3kt$gm!y37ddt4-%5)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["date29.herokuapp.com",
                  "127.0.0.1"]
